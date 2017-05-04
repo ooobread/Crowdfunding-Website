45bec531b066bf5e0daa73949ff6365380ddee44
Yang Shi <ys2843@nyu.edu>
2017-05-04 15:55:26 -0400

    Add files via upload

--- web/web/routes/project.js
+++ web/web/routes/project.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 var db = require('./database.js');
+var url = require('url');
 
-router.get('/post', function(req, res, next) {
- 	res.render('post');
+
+router.post('/result', function(req, res, next) {
+	var searchtext = req.body.searchtext;
+	var searchselect = req.body.searchselect;
+	console.log(searchtext);
+	if (searchselect == 'name'){
+		var sql = "select Pname,description from project where Pname like '%" + searchtext + "%'";
+	}
+	else if (searchselect == 'tag'){
+		var sql = "select distinct Pname,description " +
+				"from project,haveTag,Tag where project.pid = hpid and htid = tid and " +
+				"tname like '%" + searchtext + "%'";
+	}
+	else {
+		var sql = "select Pname,description from project where description like '%" + searchtext + "%'";
+	}
+	console.log(sql);
+	db.query(sql, function(err, rows){
+		  if(err){
+	      console.log('Error!');
+	  	
+	  	}
+	  	else{
+	      console.log('Success!');
+	      res.render('search',{rows,rows});
+	      
+	 	}
+		
+	  });
 });
 
 module.exports = router;
