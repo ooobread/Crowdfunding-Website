a01d6d8ef8554a69b2e681c643e1ce63c4b1b442
Yang Shi <ys2843@nyu.edu>
2017-05-06 22:21:04 -0400

    Add files via upload

--- web/web/routes/project.js
+++ web/web/routes/project.js
@@ -3,13 +3,28 @@ var router = express.Router();
 var db = require('./database.js');
 var url = require('url');
 
+router.param('pid', function(req, res, next, pid) {
+
+	req.pid = pid;
+	next();	
+});
+
+
+router.get('/:pid', function(req,res,next){
+	var sql = "select * from project,comment,user where pid = ? and pid = cpid and uid = pruid";
+	var sqlParams = [req.pid];
+	db.query(sql, sqlParams, function(results){
+	    console.log(results[0].pid);
+		res.render('project_main',{results:results});
+	});
+});
 
 router.post('/result', function(req, res, next) {
 	var searchtext = req.body.searchtext;
 	var searchselect = req.body.searchselect;
 	console.log(searchtext);
 	if (searchselect == 'name'){
-		var sql = "select Pname,description from project where Pname like '%" + searchtext + "%'";
+		var sql = "select Pname,description,pid from project where Pname like '%" + searchtext + "%'";
 	}
 	else if (searchselect == 'tag'){
 		var sql = "select distinct Pname,description " +
@@ -17,7 +32,7 @@ router.post('/result', function(req, res, next) {
 				"tname like '%" + searchtext + "%'";
 	}
 	else {
-		var sql = "select Pname,description from project where description like '%" + searchtext + "%'";
+		var sql = "select Pname,description from project,user where name =  '" +searchtext+"' and pruid = uid";
 	}
 	console.log(sql);
 	db.query(sql, function(err, rows){
