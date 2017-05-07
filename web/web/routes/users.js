2aaa6151fbd10382980a55047e636409fb229667
ooobread <lin_wei1@163.com>
2017-05-07 01:38:16 -0400

    Register finish and startpage slightly updated

--- web/web/routes/users.js
+++ web/web/routes/users.js
@@ -25,6 +25,39 @@ router.post('/checklogin', function(req, res, next) {
   });
 });
 
+router.get('/register', function(req, res, next) {
+  //console.log(req.body.inputPassword);
+  res.render('register');
+});
+
+router.post('/createuser', function(req, res, next) {
+  //console.log(req.body.inputPassword);
+  console.log(req.body.inputUsername);
+  var username = req.body.inputUsername;
+  var password = req.body.inputPassword;
+  var name = req.body.inputName;
+  var hometown = req.body.inputHometown;
+  var interest = req.body.inputInterest;
+  var sql = 'select * from user where ? in (select uid from user)';
+  var sqlparams = [username];
+  db.query(sql, sqlparams, function(results){
+    if(results != ''){
+      console.log('Register: Error!');
+      res.render('register', {error: 'error'});
+    }
+    else{
+      var sql = 'insert into user (uid, pwd, name, hometown, interest) values (?, ?, ?, ?, ?)' ;
+      var sqlparams = [username, password, name, hometown, interest];
+      db.query(sql, sqlparams, function(results){
+        console.log('Register: Success!');
+        req.session.user = username;
+        req.session.pwd = password;
+        res.redirect('/account/'+username);
+      });
+    }
+  });
+});
+
 router.get('/logout', function(req, res) {
   req.session.destroy();
   res.redirect('/');
