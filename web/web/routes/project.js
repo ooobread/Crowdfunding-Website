b279042716c2f38575032c96c444d6332ecd15d6
Yang Shi <ys2843@nyu.edu>
2017-05-07 09:22:56 -0400

    Add files via upload

--- web/web/routes/project.js
+++ web/web/routes/project.js
@@ -11,12 +11,47 @@ router.param('pid', function(req, res, next, pid) {
 
 
 router.get('/:pid', function(req,res,next){
-	var sql = "select * from project,comment,user where pid = ? and pid = cpid and uid = pruid";
+	var sql = "select * from project,user,comment where pid = ? and uid = pruid and pid = cpid";
 	var sqlParams = [req.pid];
+	
+	var arr = [];
 	db.query(sql, sqlParams, function(results){
-	    console.log(results[0].pid);
-		res.render('project_main',{results:results});
+			for(i =0;i<results.length;i++){
+				var column = {};
+				column.comment = results[i].comments;
+				column.date = results[i].date;
+				arr.push(column);
+				//console.log(arr[i]);
+			}
+			
+			var info1 = {};
+			info1.pid= results[0].pid;
+			info1.Pname= results[0].Pname;
+			info1.current_amount = results[0].current_amount;
+			info1.max_fund = results[0].max_fund;
+			//console.log(column.comment);
+			res.render('project_main',{info : arr,
+				Pname:info1.Pname,
+				current_amount :info1.current_amount,
+				max_fund :info1.max_fund,
+				pid: info1.pid
+			  });
+			
 	});
+	
+});
+
+router.get('/:pid/comment', function(req,res,next){
+	var sql = "INSERT INTO comment values (1, ?,?, now())";
+	var sqlparam = [req.pid];
+	var com = [req.query.comment];
+	var sqlparam = sqlparam.concat(com);
+	console.log(sqlparam);
+	//console.log(req.query.comment);
+	db.query(sql, sqlparam, function(results){
+		res.redirect('/project/' + req.pid);
+	});
+	
 });
 
 router.post('/result', function(req, res, next) {
